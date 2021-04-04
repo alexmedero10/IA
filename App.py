0b78e72f2ada3b95ddc81d0a2b79c8ab76bd4e26
alexmedero10 <43555049+alexmedero10@users.noreply.github.com>
2021-04-04 09:09:50 -0500

    Clase main

--- App.py
+++ App.py
@@ -1,10 +1,12 @@
 from tkinter import *
 from tkinter import messagebox
+from io import open
 from Celda import *
 from Mapa import *
 
-class App():
+class App:
 	lista = []
+	mapa = Mapa()
 
 	def __init__(self):
 		self.root = Tk()
@@ -16,61 +18,63 @@ class App():
 		self.root.config(menu=self.menubar)
 
 		self.filemenu = Menu(self.menubar,tearoff=0)
-		self.filemenu.add_command(label="ALL BLOCKED",command=self.bloquearMapa)
-		self.filemenu.add_separator()
 		self.filemenu.add_command(label="Crear tablero",command=self.pedirArchivo)
+		self.filemenu.add_separator()
+		self.filemenu.add_command(label="Bloquear mapa",command=self.mapa.bloquearMapa)
 		self.filemenu.add_separator()
 		self.filemenu.add_command(label="Bloquear celda",command=self.pedirCeldaBloquear)
 		self.filemenu.add_separator()
 		self.filemenu.add_command(label="Descubir celda",command=self.pedirCeldaDescubir)
 
 		self.menubar.add_cascade(label="ACTIONS",menu=self.filemenu)
-
 		self.root.mainloop()
 
+	def abrirArchivo(self, file,ventanaDatos):
+		ventanaDatos.destroy()
+		fichero = open(file,"r")
+		#CREA LA LISTA DEL MAPA
+		laberinto = []
+		while True:
+			linea = fichero.readline()
+			if not linea:
+				break
+			linea.rstrip("\n")
+			linea = list(linea.split())
+			laberinto.append([int(x) for x in linea])
 
-	def crearTablero(self,file,ventanaDatos):
-		ventanaDatos.destroy();
-		Mapa.mapa.clear()
-		Mapa.marcas.clear()
-		Mapa.abrirArchivo(Mapa,str(file.get()));
-		
+		self.mapa.setLaberinto(laberinto)
+		fichero.close()
+		self.crearTablero()
+
+	def crearTablero(self):
+
 		for i in self.root.grid_slaves():
 			i.grid_remove()
 			print(i.grid_info())
 
 		self.lista.clear()
-		for i in range(len(Mapa.mapa)):
+		for i in range(len(self.mapa.laberinto)):
 			self.root.rowconfigure(i, weight=1)
 
-		for j in range(len(Mapa.mapa[0])):
+		for j in range(len(self.mapa.laberinto[0])):
 			self.root.columnconfigure(j, weight=1)
 
-		for i in range(len(Mapa.mapa)):
-			linea = []
-			for j in range(len(Mapa.mapa[i])):
-				linea.append(Label(self.root,text="{},{}".format(i,j)))
-				if Mapa.mapa[i][j] == 0:
-					linea[j].configure(background="blue", borderwidth=2, relief="solid")
-				elif Mapa.mapa[i][j] == 1:
-					linea[j].configure(background="red", borderwidth=2, relief="solid")
-				if Mapa.mapa[i][j] == 2:
-					linea[j].configure(background="green", borderwidth=2, relief="solid")
-				if Mapa.mapa[i][j] == 3:
-					linea[j].configure(background="pink", borderwidth=2, relief="solid")
-				if Mapa.mapa[i][j] == 4:
-					linea[j].configure(background="orange", borderwidth=2, relief="solid")
-				linea[j].grid(row=i,column=j,sticky=S+N+E+W)
-			self.lista.append(linea)
+		for i in range(len(self.mapa.laberinto)):
+			for j in range(len(self.mapa.laberinto[i])):
+				linea = Celda(Label(self.root,text="{},{}".format(i,j)),self.mapa.laberinto[i][j],{"V":0,"O":0,"I":0,"X":0})
+				if self.mapa.laberinto[i][j] == 0:
+					linea.label.configure(background="blue", borderwidth=2, relief="solid")
+				elif self.mapa.laberinto[i][j] == 1:
+					linea.label.configure(background="red", borderwidth=2, relief="solid")
+				if self.mapa.laberinto[i][j] == 2:
+					linea.label.configure(background="green", borderwidth=2, relief="solid")
+				if self.mapa.laberinto[i][j] == 3:
+					linea.label.configure(background="pink", borderwidth=2, relief="solid")
+				if self.mapa.laberinto[i][j] == 4:
+					linea.label.configure(background="orange", borderwidth=2, relief="solid")
+				linea.label.grid(row=i,column=j,sticky=S+N+E+W)
+				self.mapa.laberinto[i][j] = linea
 
-	def bloquearMapa(self):
-		for i in range(len(Mapa.mapa)):
-			linea = []
-			for j in range(len(Mapa.mapa[i])):
-				linea.append(Label(self.root,text="{},{}".format(i,j)))
-				linea[j].configure(background="black", borderwidth=2, relief="solid")
-				linea[j].grid(row=i,column=j,sticky=S+N+E+W)
-			self.lista.append(linea)
 
 	def pedirArchivo(self):
 		self.root.deiconify()
@@ -83,7 +87,7 @@ class App():
 		d1 = StringVar()
 		Entry(ventanaDatos,justify="center",textvariable=d1).grid(row=0,column=0)
 		
-		Button(ventanaDatos,text="Crear",command=lambda:self.crearTablero(d1,ventanaDatos)).grid(row=0,column=2)
+		Button(ventanaDatos,text="Crear",command=lambda:self.abrirArchivo(str(d1.get()),ventanaDatos)).grid(row=0,column=2)
 
 	def pedirCeldaBloquear(self):
 		self.root.deiconify()
@@ -99,23 +103,8 @@ class App():
 		d2 = StringVar()
 		Entry(ventanaDatos,justify="center",textvariable=d2).grid(row=0,column=1)
 		
-		Button(ventanaDatos,text="Crear",command=lambda:self.bloquearCelda(d1,d2,ventanaDatos)).grid(row=0,column=2)
+		Button(ventanaDatos,text="Crear",command=lambda:self.mapa.laberinto[int(d1.get())][int(d2.get())].bloquearCelda(ventanaDatos)).grid(row=0,column=2)
 
-	def bloquearCelda(self,n,m,ventanaDatos):
-		ventanaDatos.destroy()
-		try:
-			n = int(n.get())
-			m = int(m.get())
-		except ValueError:
-			messagebox.showerror("Error!","Inserte numeros mayores a 0")
-			return
-
-		if n <= 0 or m <= 0:
-			messagebox.showerror("Error!","Inserte numeros mayores a 0")
-			self.pedirDatos()
-			return
-
-		self.lista[n][m].configure(background="black")
 		
 	def pedirCeldaDescubir(self):
 		self.root.deiconify()
@@ -131,31 +120,7 @@ class App():
 		d2 = StringVar()
 		Entry(ventanaDatos,justify="center",textvariable=d2).grid(row=0,column=1)
 		
-		Button(ventanaDatos,text="Crear",command=lambda:self.descubrirCelda(d1,d2,ventanaDatos)).grid(row=0,column=2)
+		Button(ventanaDatos,text="Crear",command=lambda:self.mapa.laberinto[int(d1.get())][int(d2.get())].descubrirCelda(ventanaDatos)).grid(row=0,column=2)
 
-	def descubrirCelda(self,n,m,ventanaDatos):
-		ventanaDatos.destroy()
-		try:
-			n = int(n.get())
-			m = int(m.get())
-		except ValueError:
-			messagebox.showerror("Error!","Inserte numeros mayores a 0")
-			return
-
-		if n <= 0 or m <= 0:
-			messagebox.showerror("Error!","Inserte numeros mayores a 0")
-			self.pedirDatos()
-			return
-
-		if Mapa.mapa[n][m] == 0:
-			self.lista[n][m].configure(background="blue", borderwidth=2, relief="solid")
-		elif Mapa.mapa[n][m] == 1:
-			self.lista[n][m].configure(background="red", borderwidth=2, relief="solid")
-		if Mapa.mapa[n][m] == 2:
-			self.lista[n][m].configure(background="green", borderwidth=2, relief="solid")
-		if Mapa.mapa[n][m] == 3:
-			self.lista[n][m].configure(background="pink", borderwidth=2, relief="solid")
-		if Mapa.mapa[n][m] == 4:
-			self.lista[n][m].configure(background="orange", borderwidth=2, relief="solid")
 
 app = App()
