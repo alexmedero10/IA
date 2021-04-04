8e2d3afb708bf9e741b4ce0cede5bdb172d40653
alexmedero10 <43555049+alexmedero10@users.noreply.github.com>
2021-04-04 09:11:07 -0500

    Ya está más bonito

--- Mapa.py
+++ Mapa.py
@@ -1,38 +1,25 @@
-from io import open
+class Mapa:
+	
+	def __init__(self):
+		self.laberinto = []
 
-class Mapa():
-	mapa = []
-	marcas = []
-
-	def abrirArchivo(self, archivo):
-		fichero = open(archivo,"r")
-		#CREA LA LISTA DEL MAPA
-		while True:
-			linea = fichero.readline()
-			if not linea:
-				break
-			linea.rstrip("\n")
-			linea = list(linea.split())
-			self.mapa.append([int(x) for x in linea])
-
-		#CREA LA LISTA DE MARCAS DE CADA POSICION
-		for lista in self.mapa:
-			lista_marcas = []
-			for i in range(len(lista)):
-				#SE PODRIA CREAR UN OBJETO CELDA EN LUGAR DE UN DICCIONARIO
-				lista_marcas.append({"V":0,"O":0,"I":0,"X":0})
-			self.marcas.append(lista_marcas)
-		fichero.close()
+	def bloquearMapa(self):
+		for i in range(len(self.laberinto)):
+			for celda in self.laberinto[i]:
+				celda.setLabel("black")
 
 	def mostrarValor(self):
 		#MOSTRAR VALOR
 		fila = int(input("Fila: "))
 		columna = int(input("Columna: "))
-		print(self.mapa[fila][columna])
+		print(self.laberinto[fila][columna])
 
 	def cambiarValor(self):
 		#CAMBIAR VALOR
 		fila = int(input("Fila: "))
 		columna = int(input("Columna: "))
 		terreno = int(input("Nuevo terreno: "))
-		self.mapa[fila][columna] = terreno
+		self.laberinto[fila][columna] = terreno
+
+	def setLaberinto(self, laberinto):
+		self.laberinto = laberinto
