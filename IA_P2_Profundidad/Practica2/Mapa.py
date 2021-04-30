d1a5eea2dde78c834f80f59e67797dca6c80b91f
alexmedero10 <43555049+alexmedero10@users.noreply.github.com>
2021-04-29 21:08:45 -0500

    Add files via upload

---
+++ IA_P2_Profundidad/Practica2/Mapa.py
@@ -0,0 +1,18 @@
+class Mapa:
+
+    def __init__(self):
+        self.laberinto = []
+
+    def bloquearMapa(self):
+        for i in range(len(self.laberinto)):
+            for celda in self.laberinto[i]:
+                celda.setLabel("black")
+
+    def borrarMarcas(self):
+        for i in range(len(self.laberinto)):
+            for celda in self.laberinto[i]:
+                celda.quitarMarcas()
+
+    def setLaberinto(self, laberinto):
+        self.laberinto = laberinto
+# Definir individuo
